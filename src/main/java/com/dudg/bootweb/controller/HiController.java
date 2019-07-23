676c36d57eab580df37111c90729454379104adc
dudg1021@163.com <dudg1021@163.com>
2019-07-23 23:49:09 +0800

    add project

---
+++ src/main/java/com/dudg/bootweb/controller/HiController.java
@@ -0,0 +1,13 @@
+package com.dudg.bootweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HiController {
+
+    @RequestMapping("/hi")
+    public String hi(){
+        return "index";
+    }
+}
